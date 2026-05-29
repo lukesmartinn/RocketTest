49ba6c4e7025fcb17d61fc8452e82a6c96f567a9
Martin Lukeš <martin.lukes@prorocketeers.com>
2026-05-29 14:50:48 +0200

    DNS again

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import urllib.request
 
-print("--- START METADATA TEST ---")
-
-# Tohle je standardní magická IP v cloudech (AWS, GCP, Azure, OpenStack)
-url = "http://169.254.169.254/latest/meta-data/"
+print("--- START METADATA ROOT SCAN ---")
+# Zkusíme jen základní adresu bez podadresářů
+url = "http://169.254.169.254/"
 
 try:
-    # Zkusíme tam prostě klepnout a přečíst odpověď
     with urllib.request.urlopen(url, timeout=2) as response:
-        print("!!! NAŠEL JSEM CLOUD METADATA !!!")
-        print(f"ODPOVĚĎ: {response.read().decode()}")
+        print("!!! SERVER ODPOVĚDĚL !!!")
+        print(f"DOSTUPNÉ CESTY: {response.read().decode()}")
 except Exception as e:
-    print(f"Metadata nedostupná: {e}")
+    print(f"Chyba: {e}")
 
 print("--- KONEC TESTU ---")
