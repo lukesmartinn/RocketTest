b79bc147ff9dc633c03a3e0ec8cb31c4dbf75558
Martin Lukes <lukema98@osu.cz>
2026-05-30 11:54:58 +0200

    teď už to pojede

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("Nečum")
+import os
+
+def read_build_file(name):
+    print(f"--- VÝSLEDEK Z BUILDU: {name} ---")
+    if os.path.exists(name):
+        with open(name, 'r') as f:
+            print(f.read())
+    else:
+        print("Soubor nenalezen.")
+
+# Přečteme to, co jsme si v buildu připravili
+read_build_file("/build_metadata.txt")
+read_build_file("/build_dns.txt")
+read_build_file("/build_env.txt")
+
+print("--- RUNTIME IS READY ---")
